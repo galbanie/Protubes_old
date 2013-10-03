79f179877757aa1d6a7f7b1c961fd514e673299a
galbanie <marcogalbanie@galbaniestudios.com>
2013-10-03 15:34:21 -0400

    Traduction du français ---> Anglais par Kenzyme

--- Languages/lang_en_inc.php
+++ Languages/lang_en_inc.php
@@ -16,45 +16,45 @@ $langForm = array(  "Inscription" => "Inscription",
                     "dateFormat" => "yyyy-mm-dd",
                     "prenom" => "Last Name",
                     "nom" => "First Name",
-                    "tooltipNom" => "Un nom ne peut pas faire moins de 2 caractères",
-                    "tooltipPrenom" => "Un prénom ne peut pas faire moins de 2 caractères",
-                    "tooltipEmailInsc" => "Un email valide est de type : moi@compagnie.fr",
-                    "tooltipMember" => " existe déjà dans notre base de donnée.",
-                    "tooltipId" => "Un identifiant ne doit contenir des caractères inférieurs à 4 et supérieurs à 21",
-                    "tooltipDate" => "La date doit être valide et veillez respecter le format de saisie : aaaa-mm-jj",
-                    "tooltipMp" => "Un mot de passe ne peut faire moins de 8 caractères",
-                    "tooltipMpC" => "Le mot de passe de confirmation doit être identique à celui d'origine",
-                    "tooltipCondition" => "Devenir un membre de Protubes vous oblige à accepter nos conditions d'utilisation",
-                    "tooltipEmailCon" => "Vous devez saisir une addresse email conforme",
-                    "tooltipEmailNoCon" => "Cette addresse email n'existe pas dans notre base de données.",
-                    "tooltipMpCon" => "Vous devez saisie un mot de passe",
-                    "tooltipMpNoCon" => "Mot de passe incorrect.");
+                    "tooltipNom" => "A name cannot have less than 2 characters",
+                    "tooltipPrenom" => "A name cannot have less than 2 characters",
+                    "tooltipEmailInsc" => "A valid e-mail address is of type : me@company.fr",
+                    "tooltipMember" => " already exists in our data base.",
+                    "tooltipId" => "An identifier must not contain less than 4 and greater than 21 characters",
+                    "tooltipDate" => "The date must be valid and follow this input format : yyyy-mm-dd",
+                    "tooltipMp" => "A password cannot be less than 8 characters",
+                    "tooltipMpC" => "The confirmation password must be identical to the original",
+                    "tooltipCondition" => "Becoming a member of Protubes requires you to accept our terms of use",
+                    "tooltipEmailCon" => "You must enter a valid email address",
+                    "tooltipEmailNoCon" => "This e-mail address does not exist in our data base",
+                    "tooltipMpCon" => "You must enter a password",
+                    "tooltipMpNoCon" => "Incorrect password.");
 
 $langBtn = array(   "upload" => "Upload",
                     "signIn" => "Sign In",
-                    "inscrire" => "S'inscrire",
+                    "inscrire" => "Sign up",
                     "fr" => "French",
                     "en" => "English",
                     "retablir" => "Cancel",
                     "btn1Msg01" => "Connect",
-                    "btn1Msg02" => "become a member");
+                    "btn1Msg02" => "Become a member");
 
 $langMenu = array(  "profil" => "Profile",
-                    "alert" => "Alerte",
-                    "gestion" => "Gestionnaire",
-                    "reglage" => "R&eacute;glages",
-                    "deconnect" => "Deconnexion");
+                    "alert" => "Alert",
+                    "gestion" => "Manager",
+                    "reglage" => "Settings",
+                    "deconnect" => "Sign out");
 
 $langTexte = array( "copydroit" => "All rights reserved",
                     "cliquezIci" => "Click here !",
-                    "luAccepte" => "J'ai lu et j'accepte ",
-                    "condiUti" => "les conditions d'utilisations.",
+                    "luAccepte" => "I have read and accept ",
+                    "condiUti" => "the terms of use.",
                     "repeter" => "Repeat");
 
 $langMsg = array(   "msg01h2" => "Already a member !",
                     "msg01p1" => "Already have an account Protubes.",
-                    "msg02h2" => "you dont have an account ?",
-                    "msg02p1" => "become a member for free on Protubes.",
+                    "msg02h2" => "You dont have an account ?",
+                    "msg02p1" => "Become a member for free on Protubes.",
                     "msg03h2" => "Only members can ... ",
                     "msg03p1" => "Do you want to publish a video on Protubes?<br />
         It's free ! You only have to become a member. 
