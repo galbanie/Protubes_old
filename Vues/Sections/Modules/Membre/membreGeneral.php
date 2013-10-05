77fe193eddb68fa0c1e9984a75fb9ae74a8035a2
galbanie <marcogalbanie@galbaniestudios.com>
2013-10-05 10:23:06 -0400

    Implementation de l'interface Timeline Membre > General > Timeline (en partie)

--- Vues/Sections/Modules/Membre/membreGeneral.php
+++ Vues/Sections/Modules/Membre/membreGeneral.php
@@ -2,5 +2,71 @@
     
 </div>
 <div id="membreGeneralTimeline">
-    le timeline
+    <!--div class="containerMembreAddVideo">
+        
+    </div-->
+    <ol id="timeline">
+        
+        <li>
+            <div class="enteteTimeline">
+                <a class="lien-img-membre" href="#">
+                    <img class="img-user" src="Ressources/Images/Profil_default.jpg" />
+                    <span class="name-user">Yanis</span>
+                </a>
+            </div>
+            <div class="contentTimeline">
+                
+            </div>
+        </li>
+        
+        <li>
+            <div class="enteteTimeline">
+                <a class="lien-img-membre" href="#">
+                    <img class="img-user" src="Ressources/Images/Profil_default.jpg" />
+                    <span class="name-user">Toudef</span>
+                </a>
+            </div>
+            <div class="contentTimeline">
+                <p>Video</p>
+            </div>
+        </li>
+        
+        <li>
+            <div class="enteteTimeline">
+                <a class="lien-img-membre" href="#">
+                    <img class="img-user" src="Ressources/Images/Profil_default.jpg" />
+                    <span class="name-user">Setruk Jean Samuel Bryan</span>
+                </a>
+            </div>
+            <div class="contentTimeline">
+                
+            </div>
+        </li>
+        
+        <li>
+            <div class="enteteTimeline">
+                <a class="lien-img-membre" href="#">
+                    <img class="img-user" src="Ressources/Images/Profil_default.jpg" />
+                    <span class="name-user">LeBogoss</span>
+                </a>
+            </div>
+            <div class="contentTimeline">
+                
+            </div>
+        </li>
+        
+        <li>
+            <div class="enteteTimeline">
+                <a class="lien-img-membre" href="#">
+                    <img class="img-user" src="Ressources/Images/Profil_default.jpg" />
+                    <span class="name-user">Yann</span>
+                </a>
+            </div>
+            <div class="contentTimeline">
+                
+            </div>
+        </li>
+        
+    </ol>
+    
 </div>
